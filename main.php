9e260d8730ae3a2639b397001975d706aeac7af9
Rainbow Spike <3947636+Rainbow-Spike@users.noreply.github.com>
2020-08-25 23:58:40 +1000

    Update main.php

--- main.php
+++ main.php
@@ -1,23 +1,23 @@
 <?php
 header ( 'X-UA-Compatible: IE = edge, chrome = 1' );
 echo '<!DOCTYPE html>
-<html xml:lang = "' . $conf["lang"] . '" lang = "' . $conf["lang"] . '" dir = "' . $lang['direction'] . '">
+<html xml:lang = "' . $conf [ "lang" ] . '" lang = "' . $conf [ "lang" ] . '" dir = "' . $lang [ "direction" ] . '">
 	<head>
 		<meta charset = "utf-8" />
-		<title>'; tpl_pagetitle ( ); echo ' [' . strip_tags ( $conf['title'] ) . ']</title>
-		<link rel="alternate" type="application/json+oembed" href="https://app.comicslate.org/embed.json?id='.$ID.'" />
+		<title>'; tpl_pagetitle ( ); echo ' [' . strip_tags ( $conf [ "title" ] ) . ']</title>
+		<link rel = "alternate" type = "application/json+oembed" href = "https://app.comicslate.org/embed.json?id=' . $ID . ( $REV == false ? '' : '&amp;rev='. $REV ) . '" />
 		<meta name = "viewport" content = "width = device-width, initial-scale = 1" />
-		<script src="//an.yandex.ru/system/context.js" async></script>
-		<script src="//mc.yandex.ru/metrika/tag.js" async></script>
+		<script src = "//an.yandex.ru/system/context.js" async></script>
+		<script src = "//mc.yandex.ru/metrika/tag.js" async></script>
 		<script src = "//cdn.sendpulse.com/js/push/959988d16690251d333a6f140762013e_1.js" async></script>
 		' . tpl_favicon ( array ( 'favicon', 'mobile' ) );
 tpl_metaheaders ( );
 /*echo '
-		<meta http-equiv = "Content-Security-Policy-Report-Only" content = "default-src *; script-src \'self\' \'unsafe-eval\' //cdnjs.cloudflare.com //cdn.sendpulse.com wss://realtime.services.disqus.com //disqus.com; style-src \'self\'; object-src \'self\' //youtube.com; img-src \'self\'; media-src \'self\' //youtube.com; frame-src \'self\'; font-src \'self\'; ">';*/
+		<meta http-equiv = "Content-Security-Policy-Report-Only" content = "default-src *; script-src \'self\' \'unsafe-eval\' //cdnjs.cloudflare.com //cdn.sendpulse.com; style-src \'self\'; object-src \'self\' //youtube.com; img-src \'self\'; media-src \'self\' //youtube.com; frame-src \'self\'; font-src \'self\'; ">';*/
 echo '
 	</head>
 	<body>
-		<div class = "dokuwiki lang-' . $conf["lang"] . '">';
+		<div class = "dokuwiki lang-' . $conf [ "lang" ] . '">';
 
 // HEADER
 $rlogo = time ( ) / 600 % 9;
@@ -39,7 +39,7 @@ for ( $i = 0; $i <= count ( $ilinks ) - 1; $i++ ) {
 		default : echo ' , ';
 	}
 	echo '
-					<a href = "' . $ilinks[$i][0] . '" target = ' . $ilinks[$i][2] . '>' . $ilinks[$i][1] . '</a>';
+					<a href = "' . $ilinks [ $i ] [ 0 ] . '" target = ' . $ilinks [ $i ] [ 2 ] . '>' . $ilinks [ $i ] [ 1 ] . '</a>';
 };
 echo '
 					<div class = "searcher">
@@ -49,10 +49,12 @@ echo '
 			</header>
 
 			<nav class = "translation">
-				';
+				<noindex>
+					';
 $translation = plugin_load ( 'helper', 'translation' );
 if ( $translation ) echo $translation -> showTranslations ( );
 echo '
+				</noindex>
 			</nav>';
 
 // MAIN
@@ -85,15 +87,13 @@ tpl_flush ( );
 echo '
 			<noindex>
 				<aside id = "pagetools">
-					<ul>
-						' . ( new \dokuwiki\Menu\PageMenu ( ) ) -> getListItems ( ) . '
+					<ul>' . ( new \dokuwiki\Menu\PageMenu ( ) ) -> getListItems ( ) . '
 					</ul>
 				</aside>
 			</noindex>';
 
 // FOOTER
 echo '
-			<div id = "yandex_rtb"></div>
 			<footer>';
 
 if ( $conf['breadcrumbs'] ) {
@@ -105,7 +105,9 @@ if ( $conf['breadcrumbs'] ) {
 				</noindex>';
 };
 echo $apps . '
-				<div class = "count">
+				<div class = "count">';
+if ( $ACT != 'edit' ) {
+	echo '
 					<a href = "//metrika.yandex.ru/stat/?id=25500497&amp;from=informer" target = "_blank" rel = "nofollow">
 						<img src = "//informer.yandex.ru/informer/25500497/3_0_7BC9F7FF_5BA9D7FF_0_pageviews" alt = "Яндекс.Метрика" title = "Яндекс.Метрика: данные за сегодня (просмотры, визиты и уникальные посетители)" class = "ym-advanced-informer" data-cid = "25500497" data-lang = "ru" />
 					</a>
@@ -113,23 +115,10 @@ echo $apps . '
 						<div>
 							<img src = "//mc.yandex.ru/watch/25500497" alt = "" />
 						</div>
-					</noscript>
-				</div>';
-/*<script src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js" async></script>
-<script>
-	(adsbygoogle = window.adsbygoogle || []).push({
-		google_ad_client: "ca-pub-1929476284872276",
-		enable_page_level_ads: true
-	});
-</script>
-*/
-/* более новый вид Google AdSense
-<script src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js" async></script>
-<ins class="adsbygoogle" style = "display: inline-block; width: 728px; height: 90px;" data-ad-client = "ca-pub-1929476284872276" data-ad-slot = "1234567890"></ins>
-<script>(adsbygoogle = window.adsbygoogle || []).push({});</script>
-*/
-
+					</noscript>';
+};
 echo '
+				</div>
 				<div class = "social">
 					';
 $socbut = array (
@@ -205,6 +194,7 @@ if ( !empty ( $_SERVER['REMOTE_USER'] ) ) {
 // END
 echo '
 			</footer>
+			<!--<div id = "yandex_rtb"></div>-->
 		</div>';
 if ( ( $ACT == 'edit' ) || ( $ACT == 'preview' ) ) {
 	echo '
