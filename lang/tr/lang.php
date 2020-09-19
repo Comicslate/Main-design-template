c4089eb1377c20a638bb4d3ad9ed0ebe767c6fbb
Rainbow Spike <3947636+Rainbow-Spike@users.noreply.github.com>
2020-09-19 23:13:09 +1000

    Create lang.php

---
+++ lang/tr/lang.php
@@ -0,0 +1,10 @@
+<?php
+
+$lang['start']              = 'Ana Sayfa';
+$lang['menu']               = 'Tüm çizgi romanlar';
+$lang['news']               = 'Haberler';
+$lang['helproom']           = 'Yardım odası';
+$lang['chat']               = 'Sohbet';
+$lang['RSS']                = 'Sitenin RSS';
+$lang['RSSpart']            = 'Bölüm RSS';
+$lang['RSSnew']             = 'yeni sayfalar';
