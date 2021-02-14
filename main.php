8f26153b7082b8e11aeaac890f8720a98db5ac8d
Rainbow Spike <3947636+Rainbow-Spike@users.noreply.github.com>
2021-02-14 21:16:37 +1000

    Индент и прелоад
    индент всего кода
    преконнект к гуглопапке
    прелоад гуглошрифтов по языку
    прелоад дат-фест-комика и фонт-осома

--- main.php
+++ main.php
@@ -1,30 +1,17 @@
 <?php
-header ( 'X-UA-Compatible: IE = edge, chrome = 1' );
-echo '<!DOCTYPE html><html xml:lang = "' . $conf [ "lang" ] . '" lang = "' . $conf [ "lang" ] . '" dir = "' . $lang [ "direction" ] . '">';
+header ( 'X-UA-Compatible: IE = edge, chrome = 1' ); 
 
-echo '<head><meta charset = "utf-8" /><title>'; tpl_pagetitle ( ); echo ' [' . strip_tags ( $conf [ "title" ] ) . ']</title><link rel = "alternate" type = "application/json+oembed" href = "https://app.comicslate.org/embed.json?id=' . $ID . ( $REV == false ? '' : '&amp;rev='. $REV ) . '" /><meta name = "viewport" content = "width = device-width, initial-scale = 1" /><script src = "//mc.yandex.ru/metrika/tag.js" async></script>';
-if ( ( $ACT == 'edit' ) || ( $ACT == 'preview' ) ) {
-	echo '<script charset = "UTF-8" src = "/lib/plugins/cotan/editor.js?ver=' . date ( "y-m-d_H:i:s", filemtime ( '/var/www/comicslate.org/lib/plugins/cotan/editor.js' ) ) . '" defer></script>';
-}
-echo tpl_favicon ( array ( 'favicon', 'mobile' ) );
-tpl_metaheaders ( );
-$gfonts = array (
-	'Frank+Ruhl+Libre:wght@500',
-	'Jua',
-	'Kosugi+Maru',
-	'ZCOOL+KuaiLe',
-	'Inknut+Antiqua'
-);
-for ( $i = 0; $i <= count ( $gfonts ) - 1; $i++ ) {
-	echo '<link href = "https://fonts.googleapis.com/css2?family=' . $gfonts [ $i ] . '&amp;display=swap" rel = "stylesheet">
-';
+switch ( $conf [ "lang" ] ) {
+	case 'he' : $dfont = 'Frank+Ruhl+Libre:wght@700'; break;
+	case 'hi' : $dfont = 'Inknut+Antiqua'; break;
+	case 'ko' : $dfont = 'Jua'; break;
+	case 'ja' : $dfont = 'Kosugi+Maru'; break;
+	case 'zh' : $dfont = 'ZCOOL+KuaiLe'; break;
+	default : break;
 };
 
-echo '</head><body><div class = "dokuwiki lang-' . $conf [ "lang" ] . '">';
+$rlogo = time ( ) / 300 % 9;
 
-// HEADER
-$rlogo = time ( ) / 600 % 9;
-echo '<header class = "logo' . $rlogo . '"><div class="top-texts">';
 $ilinks = array (
 	array ( '/'. $conf [ "lang" ] . '/start',		tpl_getLang ( 'start' ),	'_self' ),
 	array ( '/'. $conf [ "lang" ] . '/menu',		tpl_getLang ( 'menu' ),		'_self' ),
@@ -33,73 +20,152 @@ $ilinks = array (
 	array ( '//discord.gg/T8p6M4Q',					tpl_getLang ( 'chat' ),		'_blank' ),
 	array ( '//app.comicslate.org/',				'Mobile App',				'_blank' ),
 );
+
+$translation = plugin_load ( 'helper', 'translation' );
+$trans = ( $translation ) ? $t4 . $translation -> showTranslations ( ) : '';
+
+$apps = "\t\t\t\t" . '<div class = "apps"><a href = "//play.google.com/store/apps/details?id=org.dasfoo.comicslate&amp;utm_source=comicslate-org" target = "_blank"><img src = "' . DOKU_TPL . 'images/googleapp_optim.svg" alt = "Get it on Google Play"></a><a href = "//apps.apple.com/ru/app/comicslate/id1485894069" target = "_blank"><img src = "' . DOKU_TPL . 'images/appstore_optim.svg" alt = "Download on the App Store"></a></div>' . $t4;
+
+$socbut = array (
+	array ( '/feed.php', tpl_getLang ( 'RSS' ), 'rss' ),
+	array ( '/feed.php?onlynewpages=1', tpl_getLang ( 'RSS' ) . ' (' . tpl_getLang ( 'RSSnew' ) . ')', 'rss' ),
+	array ( '//patreon.com/comicslate', 'Patreon', 'patreon' ),
+	array ( '//discord.gg/T8p6M4Q', 'Discord', 'discord' ),
+	array ( '//t.me/comicslate', 'Telegram', 'telegram' ),
+	array ( '//www.reddit.com/r/Comicslate', 'Reddit', 'reddit' ),
+	array ( '//rainbow-spike.tumblr.com', 'Tumblr', 'tumblr' ),
+	array ( '//facebook.com/groups/comicslate', 'Facebook', 'facebook' ),
+	array ( '//twitter.com/Rainbow_Spike', 'Twitter', 'twitter' ),
+	array ( '//vk.com/comicslate', 'VKontakte', 'vkontakte' ),
+	array ( '/feed.php?mode=recent&amp;ns=' . $INFO [ 'namespace' ], tpl_getLang ( 'RSSpart' ), 'rss' ),
+	array ( '/feed.php?mode=recent&amp;ns=' . $INFO [ 'namespace' ] . '&amp;onlynewpages=1', tpl_getLang ( 'RSSpart' ) . ' (' . tpl_getLang ( 'RSSnew' ) . ')', 'rss' )
+);
+$t2 = "\n\t\t"; $t3 = "\n\t\t\t"; $t4 = "\n\t\t\t\t"; $t5 = "\n\t\t\t\t\t"; $t6 = "\n\t\t\t\t\t\t";
+
+
+
+
+
+
+
+
+echo '<!DOCTYPE html>' .
+"\n" . '<html xml:lang = "' . $conf [ "lang" ] . '" lang = "' . $conf [ "lang" ] . '" dir = "' . $lang [ "direction" ] . '">' .
+
+"\n\t" . '<head>' . // HEAD
+$t2 . '<meta charset = "utf-8" />' .
+$t2 . '<title>' .
+$t3 ; tpl_pagetitle ( ); echo ' [Comicslate]' .
+$t2 . '</title>' .
+$t2 . '<link rel = "alternate" type = "application/json+oembed" href = "https://app.comicslate.org/embed.json?id=' . $ID . ( $REV == false ? '' : '&amp;rev='. $REV ) . '" />' .
+$t2 . '<meta name = "viewport" content = "width = device-width, initial-scale = 1" />' .
+$t2 . '<script src = "//mc.yandex.ru/metrika/tag.js" async></script>';
+if ( ( $ACT == 'edit' ) || ( $ACT == 'preview' ) )
+	echo $t2 . '<script charset = "UTF-8" src = "/lib/plugins/cotan/editor.js?ver=' . date ( "y-m-d_H:i:s", filemtime ( '/var/www/comicslate.org/lib/plugins/cotan/editor.js' ) ) . '" defer></script>';
+echo $t2 . tpl_favicon ( array ( 'favicon', 'mobile' ) ) .
+$t2 . '<link rel = "preconnect" href="https://fonts.gstatic.com" />';
+if ( preg_match ( '/(h[ei]|ko|ja|zh)/', $conf [ "lang" ] ) )
+	echo  $t2 . '<link rel = "preload" href = "https://fonts.googleapis.com/css2?family=' . $dfont . '&amp;display=swap" as = "style" crossorigin = "anonymous">';
+echo $t2 . '<link rel = "preload" href = "/lib/tpl/comicslate/fonts/dat_fest_comic.otf" as = "font" type = "font/otf" crossorigin = "anonymous">' .
+$t2 . '<link rel = "preload" href = "/lib/tpl/comicslate/fonts/font_awesome.otf" as = "font" type = "font/otf" crossorigin = "anonymous">' .
+$t2; tpl_metaheaders ( );
+echo "\n\t" . '</head>' .
+
+"\n\t" . '<body>' . // BODY
+$t2 . '<div class = "dokuwiki lang-' . $conf [ "lang" ] . '">';
+
+echo $t3 . '<header class = "logo' . $rlogo . '">' . // MENU1
+$t4 . '<div class = "top-texts">'; // Topline
 for ( $i = 0; $i <= count ( $ilinks ) - 1; $i++ ) {
 	switch ( $i ) {
 		case 0 : break;
 		default : echo ' , ';
 	}
-	echo '<a href = "' . $ilinks [ $i ] [ 0 ] . '" target = ' . $ilinks [ $i ] [ 2 ] . '>' . $ilinks [ $i ] [ 1 ] . '</a>';
+	echo $t5 . '<a href = "' . $ilinks [ $i ] [ 0 ] . '" target = ' . $ilinks [ $i ] [ 2 ] . '>' . $ilinks [ $i ] [ 1 ] . '</a>';
 };
-echo '</div>';
-echo '<div class = "searcher">'; tpl_searchform ( ); echo '</div>';
-echo '<div class="newlogo-wrap"><div class = "newlogo"></div></div>';
-echo '</header>';
+echo $t4 . '</div>' .
+$t4 . '<div class = "searcher">' . // Search
+$t5 ; tpl_searchform ( );
+echo $t4 . '</div>' .
+$t4 . '<div class = "newlogo-wrap">' . // Logo
+$t5 . '<div class = "newlogo"></div>' .
+$t4 . '</div>' .
+$t3 . '</header>' .
 
-// TRANSLATION
-echo '<nav class = "translation">';
-$translation = plugin_load ( 'helper', 'translation' );
-if ( $translation )
-	echo $translation -> showTranslations ( );
-echo '</nav>';
+$t3 . '<nav class = "translation">' . $trans . $t3 . '</nav>' . // MENU2
 
-// CHECKBOXES
-echo '<div id = "translabel"></div>';
-if ( !empty ( html_msgarea ( ) ) ) {
-	echo '<div class = "meta">'; html_msgarea ( ); echo '</div>';
+$t3 . '<div id = "translabel"></div>'; // CHECKBOX
+
+if ( !empty ( html_msgarea ( ) ) ) { // MESSAGE
+	echo $t3 . '<div class = "meta">' . $t4;
+	html_msgarea ( );
+	echo $t3 . '</div>';
 };
 
-// MAIN
-$apps = '<div class = "apps"><a href = "//play.google.com/store/apps/details?id=org.dasfoo.comicslate&amp;utm_source=comicslate-org" target = "_blank"><img src = "' . DOKU_TPL . 'images/googleapp_optim.svg" alt = "Get it on Google Play"></a><a href = "//apps.apple.com/ru/app/comicslate/id1485894069" target = "_blank"><img src = "' . DOKU_TPL . 'images/appstore_optim.svg" alt = "Download on the App Store"></a></div>';
-tpl_flush ( ); echo '<article class = "page">' . $apps; tpl_content ( ); echo '</article>'; tpl_flush ( );
+tpl_flush ( );
+echo $t3 . '<article class = "page">' . // PAGE
+"\n"; $apps; // App buttons top
+tpl_content ( );
+echo $t3 . '</article>';
+tpl_flush ( );
 
-// SiDEBAR
-echo '<noindex><aside id = "pagetools"><ul>' . ( new \dokuwiki\Menu\PageMenu ( ) ) -> getListItems ( ) . '</ul></aside></noindex>';
+echo $t3 . '<noindex>' .
+$t4 . '<aside id = "pagetools">' . // SIDEBAR
+$t5 . '<ul>' .
+$t6 . ( new \dokuwiki\Menu\PageMenu ( ) ) -> getListItems ( ) .
+$t5 . '</ul>' .
+$t4 . '</aside>' .
+$t3 . '</noindex>';
 
-// FOOTER
-echo '<footer><noindex>';
-if ( $conf['breadcrumbs'] ) {
-	echo '<div class = "breadcrumbs">'; tpl_breadcrumbs ( ); echo '</div>';
+echo $t3 . '<footer>' . // FOOTER
+$t4 . '<noindex>';
+
+if ( $conf [ 'breadcrumbs' ] ) { // Breadcrumbs
+	echo $t5 . '<div class = "breadcrumbs">';
+	tpl_breadcrumbs ( );
+	echo '</div>';
 };
-echo '<div class = "social">';
-$rss_new = ' (' . tpl_getLang ( 'RSSnew' ) . ')';
-$feed_rec = '/feed.php?mode=recent&amp;ns=';
-$socbut = array (
-	array ( '/feed.php',												tpl_getLang ( 'RSS' ),					'rss' ),
-	array ( '/feed.php?onlynewpages=1',									tpl_getLang ( 'RSS' ) . $rss_new,		'rss' ),
-	array ( '//patreon.com/comicslate',									'Patreon',								'patreon' ),
-	array ( '//discord.gg/T8p6M4Q',										'Discord',								'discord' ),
-	array ( '//t.me/comicslate',										'Telegram',								'telegram' ),
-	array ( '//www.reddit.com/r/Comicslate',							'Reddit',								'reddit' ),
-	array ( '//rainbow-spike.tumblr.com',								'Tumblr',								'tumblr' ),
-	array ( '//facebook.com/groups/comicslate',							'Facebook',								'facebook' ),
-	array ( '//twitter.com/Rainbow_Spike',								'Twitter',								'twitter' ),
-	array ( '//vk.com/comicslate',										'VKontakte',							'vkontakte' ),
-	array ( $feed_rec . $INFO [ 'namespace' ],							tpl_getLang ( 'RSSpart' ),				'rss' ),
-	array ( $feed_rec . $INFO [ 'namespace' ] . '&amp;onlynewpages=1',	tpl_getLang ( 'RSSpart' ) . $rss_new,	'rss' )
-);
+
+echo $t5 . '<div class = "social">'; // Social
 for ( $i = 0; $i <= count ( $socbut ) - 1; $i++ ) {
-	echo '<a href = "' . $socbut [ $i ] [ 0 ] . '" class = "media ' . $socbut [ $i ] [ 2 ] . '" title = "' . $socbut [ $i ] [ 1 ] . '" target = "_blank"></a>';
+	echo $t6 . '<a href = "' . $socbut [ $i ] [ 0 ] . '" class = "media ' . $socbut [ $i ] [ 2 ] . '" title = "' . $socbut [ $i ] [ 1 ] . '" target = "_blank"></a>';
 };
-echo '</div>'. $apps;
-if ( $ACT != 'edit' )
-	echo '<div class = "count"><a href = "//metrika.yandex.ru/stat/?id=25500497&amp;from=informer" target = "_blank" rel = "nofollow"><img src = "//informer.yandex.ru/informer/25500497/3_0_7BC9F7FF_5BA9D7FF_0_pageviews" alt = "Яндекс.Метрика" title = "Яндекс.Метрика: данные за сегодня (просмотры, визиты и уникальные посетители)" class = "ym-advanced-informer" data-cid = "25500497" data-lang = "ru" /></a><noscript><div><img src = "//mc.yandex.ru/watch/25500497" alt = "" /></div></noscript></div>';
-if ( $INFO [ 'ismanager' ] ) {
-	echo '<div class = "pageinfo">'; tpl_pageinfo ( ); echo '</div>';
-};
-if ( !empty ( $_SERVER['REMOTE_USER'] ) ) {
-	echo '<div class = "user">'; tpl_userinfo ( ); echo '</div>';
-};
-echo '</noindex></footer>';
+echo $t5 . '</div>' .
 
-// END
-echo '<!--<div id = "yandex_rtb"></div>--></div><div class = "no">'; tpl_indexerWebBug ( ); echo '</div></body></html>'; ?>
+"\n\t" . $apps . "\t"; // App buttons bottom
+
+if ( $ACT != 'edit' ) {
+	echo $t5 . '<div class = "count">' . // Yandex Counter
+	$t6 . '<a href = "//metrika.yandex.ru/stat/?id=25500497&amp;from=informer" target = "_blank" rel = "nofollow">' .
+	$t6 . "\t" . '<img src = "//informer.yandex.ru/informer/25500497/3_0_7BC9F7FF_5BA9D7FF_0_pageviews" alt = "Яндекс.Метрика" title = "Яндекс.Метрика: данные за сегодня (просмотры, визиты и уникальные посетители)" class = "ym-advanced-informer" data-cid = "25500497" data-lang = "ru" />' .
+	$t6 . '</a>' .
+	$t6 . '<noscript>' .
+	$t6 . "\t" . '<div>' .
+	$t6 . "\t\t" . '<img src = "//mc.yandex.ru/watch/25500497" alt = "" />' .
+	$t6 . "\t" . '</div>' .
+	$t6 . '</noscript>' .
+	$t5 . '</div>';
+};
+
+if ( $INFO [ 'ismanager' ] ) { // Pageinfo
+	echo $t5 . '<div class = "pageinfo">' .
+	$t6; tpl_pageinfo ( );
+	echo $t5 . '</div>';
+};
+
+if ( !empty ( $_SERVER['REMOTE_USER'] ) ) { // Userinfo
+	echo $t5 . '<div class = "user">' .
+	$t6; tpl_userinfo ( );
+	echo $t5 . '</div>';
+};
+
+echo $t4 . '</noindex>' .
+$t3 . '</footer>';
+
+echo $t3 . '<!--<div id = "yandex_rtb"></div>-->' . // END
+$t2 . '</div>' .
+$t2 . '<div class = "no">' .
+$t3; tpl_indexerWebBug ( ); // Indexer
+echo $t2 . '</div>' .
+"\n\t" . '</body>' .
+"\n" . '</html>';
