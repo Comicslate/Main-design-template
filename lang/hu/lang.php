e55f991ec6d61985aff6ba8a0fb8cd9751dd3352
Rainbow Spike <3947636+Rainbow-Spike@users.noreply.github.com>
2021-05-11 05:23:26 +1000

    Add files via upload

---
+++ lang/hu/lang.php
@@ -0,0 +1,10 @@
+<?php
+
+$lang['start']				= 'Főoldal';
+$lang['menu']			= 'Minden képregény';
+$lang['news']			= 'Hírek';
+$lang['helproom']		= 'Segédszoba';
+$lang['chat']				= 'Chat';
+$lang['RSS']				= 'Site\'s RSS';
+$lang['RSSpart']		= 'Section\'s RSS';
+$lang['RSSnew']		= 'új oldalak';
