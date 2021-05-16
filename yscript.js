0c135a2c86964001e11ac5a4e991abad35b1880e
Rainbow Spike <3947636+Rainbow-Spike@users.noreply.github.com>
2021-05-16 19:31:33 +1000

    Add files via upload

---
+++ yscript.js
@@ -0,0 +1,18 @@
+(
+	function ( ) {
+		window [ "ym" ] = window [ "ym" ] || function ( ) {
+			( window [ "ym" ] . a = window [ "ym" ] . a || [ ] ) . push ( arguments )
+		};
+		window [ "ym" ] . l = 1 * new Date ( );
+	}
+) ( );
+
+ym (
+	25500497,
+	"init", {
+		clickmap: true,
+		trackLinks: true,
+		accurateTrackBounce: true,
+		webvisor: false
+	}
+);
